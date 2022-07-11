42edb6cc334addb6f5fa5e865569605bccd6feb9
Daniel Suen <danielsuen200791357@gmail.com>
2022-07-11 00:49:18 +0000

    added functions files

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-let x = 1;
-
-while (x < 20) {
-    switch (x){
-        case (x % 3 === 0 && x % 5 === 0):
-        console.log("JuliaJames");
-        case x % 3 === 0:
-        console.log("Julia");
-        case x % 5 === 0:
-        console.log("James");
-    }
-    x++;
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
 }
+
+
+
+emotions  ("happy",laugh(num){
+  let x = "";
+  for (let i = 1; i <= num; i++){
+      x = x + "ha";
+      }
+  x = x + "!";
+  return x;
+  });
