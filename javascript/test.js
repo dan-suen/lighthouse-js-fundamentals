e56f2dda5a3205547d0d896a7aa6ce5d420396e0
Daniel Suen <danielsuen200791357@gmail.com>
2022-07-25 00:47:26 +0000

    all

--- javascript/test.js
+++ javascript/test.js
@@ -1,41 +1,20 @@
-const chooseRecipe = function(bakeryA, bakeryB, recipes) {
-  // Code here!
-}
-
-let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
-let bakeryB = ['milk', 'butter', 'cream cheese'];
-let recipes = [
-    {
-        name: 'Coconut Sponge Cake',
-        ingredients: ['coconut', 'cake base']
-    },
-    {
-        name: 'Persian Cheesecake',
-        ingredients: ['saffron', 'cream cheese']
-    },
-    {
-        name: 'Custard Surprise',
-        ingredients: ['custard', 'ground beef']
+const organizeInstructors = function(instructors) {
+    let obj = {};
+    for (let i = 0; i < instructors.length; i++){
+        obj[`${instructors[i].course}`] = instructors[i].name;
     }
-];
-
-console.log(chooseRecipe(bakeryA, bakeryB, recipes));
-
-bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-bakeryB = ['red bean', 'dijon mustard', 'apples'];
-recipes = [
-    {
-        name: 'Potato Ganache',
-        ingredients: ['potatoes', 'chocolate']
-    },
-    {
-        name: 'Sweet Fish',
-        ingredients: ['anchovies', 'honey']
-    },
-    {
-        name: "Nima's Famous Dijon Raisins",
-        ingredients: ['dijon mustard', 'raisins']
-    }
-];
-
-console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+    return obj.write("\n");
+  };
+  
+  console.log(organizeInstructors([
+    {name: "Samuel", course: "iOS"},
+    {name: "Victoria", course: "Web"},
+    {name: "Karim", course: "Web"},
+    {name: "Donald", course: "Web"}
+  ]));
+  console.log(organizeInstructors([
+    {name: "Brendan", course: "Blockchain"},
+    {name: "David", course: "Web"},
+    {name: "Martha", course: "iOS"},
+    {name: "Carlos", course: "Web"}
+  ]));
